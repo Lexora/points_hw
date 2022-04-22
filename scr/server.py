2d502fa138d9255338dd54245ffeba0c188a284a
alejo <yayomx@gmail.com>
2022-04-22 04:58:54 -0400

    Almost fixed docker image - need to add tests to server added config to pyproject.toml to lint w/ black prior to dickerimg is built

--- scr/server.py
+++ scr/server.py
@@ -2,9 +2,10 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+
 def get_db():
     pass
-    #return client.application_fake_database
+    # return client.application_fake_database
 
 
 @app.route("/")
@@ -12,24 +13,29 @@ def base_route():
     return "hey there!"
 
 
-
 @app.route("/ping")
 def ping():
     return "pong"
 
-@app.route("/results", methods=['GET', 'POST'])
+
+@app.route("/results", methods=["GET", "POST"])
 def users_route():
     try:
-        if request.method == 'GET':
+        if request.method == "GET":
             pass
             # return jsonify({"status": "success", "payload": str(val)}), 200
-        elif request.method == 'POST':
+        elif request.method == "POST":
             pass
             # return jsonify({"status": "success", "payload": str(val)}), 200
     except Exception:
         return (
-            jsonify({"status": "failed", "payload": "Internal server error. Please try again later"}),
-            500
+            jsonify(
+                {
+                    "status": "failed",
+                    "payload": "Internal server error. Please try again later",
+                }
+            ),
+            500,
         )
 
 
--- tests/test_server.py
+++ tests/test_server.py
@@ -1,6 +1,8 @@
 from scr import server
 
+
 def test_server_ping():
-    response = server.app.test_client().get('/ping')
-    res = response.data.decode('utf-8') 
-    assert res == 'pong'
+    response = server.app.test_client().get("/ping")
+    res = response.data.decode("utf-8")
+    assert res == "pong"
+
